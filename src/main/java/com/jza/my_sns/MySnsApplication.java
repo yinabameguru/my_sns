565f3c284583ff50d874d7002aff3e7dc3196b9f
yinaba_meguru <764904418@qq.com>
2018-07-10 08:50:51 +0800

    create

---
+++ src/main/java/com/jza/my_sns/MySnsApplication.java
@@ -0,0 +1,12 @@
+package com.jza.my_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySnsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jza/my_sns/MySnsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jza.my_sns;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySnsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
