6fc2e502d94f5639105af844e5600acbc5200243
yinaba_meguru <764904418@qq.com>
2018-08-28 19:18:39 +0800

    version1

---
+++ src/main/java/com/jza/controller/SearchController.java
@@ -0,0 +1,66 @@
+package com.jza.controller;
+
+import com.jza.model.EntityType;
+import com.jza.model.Question;
+import com.jza.model.ViewObject;
+import com.jza.service.FollowService;
+import com.jza.service.QuestionService;
+import com.jza.service.SearchService;
+import com.jza.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SearchController {
+    private static final Logger logger = LoggerFactory.getLogger(SearchController.class);
+    @Autowired
+    SearchService searchService;
+
+    @Autowired
+    FollowService followService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    QuestionService questionService;
+
+    @RequestMapping(path = {"/search"}, method = {RequestMethod.GET})
+    public String search(Model model, @RequestParam("q") String keyword,
+                         @RequestParam(value = "offset", defaultValue = "0") int offset,
+                         @RequestParam(value = "count", defaultValue = "10") int count) {
+        try {
+            List<Question> questionList = searchService.searchQuestion(keyword, offset, count,
+                    "<em>", "</em>");
+            List<ViewObject> vos = new ArrayList<>();
+            for (Question question : questionList) {
+                Question q = questionService.getQuestionById(question.getId());
+                ViewObject vo = new ViewObject();
+                if (question.getContent() != null) {
+                    q.setContent(question.getContent());
+                }
+                if (question.getTitle() != null) {
+                    q.setTitle(question.getTitle());
+                }
+                vo.set("question", q);
+                vo.set("followCount", followService.getFollowerCount(EntityType.QUESTION.ordinal(), question.getId()));
+                vo.set("user", userService.findUser(q.getUserId()));
+                vos.add(vo);
+            }
+            model.addAttribute("vos", vos);
+            model.addAttribute("keyword", keyword);
+        } catch (Exception e) {
+            logger.error("搜索评论失败" + e.getMessage());
+        }
+        return "result";
+    }
+}
